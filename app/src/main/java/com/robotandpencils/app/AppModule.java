50aefb3200693d730c2136bf7d4a678d5c8742b2
Phillip Wray <phillip.wray@robotsandpencils.com>
2017-10-02 11:31:10 -0600

    Provide injector for MainActivity.

--- app/src/main/java/com/robotandpencils/app/AppModule.java
+++ app/src/main/java/com/robotandpencils/app/AppModule.java
@@ -1,7 +1,16 @@
 package com.robotandpencils.app;
 
+import android.app.Activity;
+
+import com.robotandpencils.app.ui.main.MainActivity;
+import com.robotandpencils.app.ui.main.MainActivitySubcomponent;
+
+import dagger.Binds;
 import dagger.Module;
+import dagger.android.ActivityKey;
 import dagger.android.AndroidInjectionModule;
+import dagger.android.AndroidInjector;
+import dagger.multibindings.IntoMap;
 
 /**
  * Created by pwray on 2017-09-27.
@@ -9,6 +18,14 @@ import dagger.android.AndroidInjectionModule;
  * The app module includes bindings to ensure the usability of `dagger.android` framework classes.
  */
 
-@Module(includes = AndroidInjectionModule.class)
+@Module(includes = AndroidInjectionModule.class,
+subcomponents = MainActivitySubcomponent.class)
 abstract class AppModule {
+
+    // TODO (ContributesAndroidInjector) remove this in favour of @ContributesAndroidInjector.
+    @Binds
+    @IntoMap
+    @ActivityKey(MainActivity.class)
+    abstract AndroidInjector.Factory<? extends Activity>
+    mainActivityInjectorFactory(MainActivitySubcomponent.Builder builder);
 }
