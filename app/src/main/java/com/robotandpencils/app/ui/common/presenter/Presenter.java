c10c7dba2c05ac0b5df2516f4897be9fe9648d2d
Phillip Wray <phillip.wray@robotsandpencils.com>
2017-10-16 11:47:21 -0600

    Setting up the MVP framework.

---
+++ app/src/main/java/com/robotandpencils/app/ui/common/presenter/Presenter.java
@@ -0,0 +1,47 @@
+package com.robotandpencils.app.ui.common.presenter;
+
+import android.os.Bundle;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created by pwray on 2017-10-16.
+ */
+
+public interface Presenter {
+
+    /**
+     * Starts the presentation.  This should be called in the view's (Activity or Fragment)
+     * onCreate() or onViewStateRestored() method respectively.
+     *
+     * @param savedInstanceState the saved instance state that contains state saved in
+     *                           {@link #onSaveInstanceState(Bundle}
+     */
+    void onStart(@Nullable Bundle savedInstanceState);
+
+    /**
+     * Resumes the presentation.  This should be called in the view's (Activity or Fragment)
+     * onResume() method.
+     */
+    void onResume();
+
+    /**
+     * Pauses the presentation.  This should be called in the view's (Activity or Fragment)
+     * onPause() method.
+     */
+    void onPause();
+
+    /**
+     * Save the state of the presentation (if any).  This should be called in the view's
+     * (Activity or Fragment) onSaveInstanceState().
+     *
+     * @param outState the out state to save instance state
+     */
+    void onSaveInstanceState(Bundle outState);
+
+    /**
+     * End the presentation.  This should be called in the view's (Activity or Fragment)
+     * onDestroy() or onDestroyView() respectively.
+     */
+    void onEnd();
+}
---
+++ app/src/main/java/com/robotandpencils/app/ui/common/view/MVPView.java
@@ -0,0 +1,11 @@
+package com.robotandpencils.app.ui.common.view;
+
+import com.robotandpencils.app.ui.common.presenter.Presenter;
+
+/**
+ * The type of all views.
+ * <p>
+ * Note that this is not named "View" because it collides with Android's {@link android.view.View}.
+ */
+public interface MVPView {
+}
